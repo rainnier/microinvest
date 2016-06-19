552719ddad558ece46ea5c956bfb7d01a5d44d71
Giancarlo Angulo <igan.long@gmail.com>
2016-06-19 12:30:54 +0800

    Refactoring
    Changing todo app to microinvest app

---
+++ src/main/java/com/jar/microinvest/HoldingResource.java
@@ -0,0 +1,42 @@
+package com.jar.microinvest;
+
+import com.todoapp.JsonTransformer;
+
+import static spark.Spark.*;
+
+/**
+ * Created by shekhargulati on 09/06/14.
+ */
+public class HoldingResource {
+
+    private static final String API_CONTEXT = "/api/v1";
+
+    private final HoldingService holdingService;
+
+    public HoldingResource(HoldingService holdingService) {
+        this.holdingService = holdingService;
+        setupEndpoints();
+    }
+
+    private void setupEndpoints() {
+        post(API_CONTEXT + "/holdings", "application/json", (request, response) -> {
+            holdingService.createNewHoldings(request.body());
+            response.status(201);
+            return response;
+        }, new com.todoapp.JsonTransformer());
+
+        get(API_CONTEXT + "/holdings/:id", "application/json", (request, response)
+
+                -> holdingService.find(request.params(":id")), new com.todoapp.JsonTransformer());
+
+        get(API_CONTEXT + "/holdings", "application/json", (request, response)
+
+                -> holdingService.findAll(), new com.todoapp.JsonTransformer());
+
+        put(API_CONTEXT + "/holdings/:id", "application/json", (request, response)
+
+                -> holdingService.update(request.params(":id"), request.body()), new JsonTransformer());
+    }
+
+
+}
---
+++ src/main/java/com/jar/microinvest/HoldingService.java
@@ -0,0 +1,48 @@
+package com.jar.microinvest;
+
+import com.google.gson.Gson;
+import com.mongodb.*;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ */
+public class HoldingService {
+
+    private final DB db;
+    private final DBCollection collection;
+
+    public HoldingService(DB db) {
+        this.db = db;
+        this.collection = db.getCollection("holdings");
+    }
+
+    public List<Holding> findAll() {
+        List<Holding> holdings = new ArrayList<>();
+        DBCursor dbObjects = collection.find();
+        while (dbObjects.hasNext()) {
+            DBObject dbObject = dbObjects.next();
+            holdings.add(new Holding((BasicDBObject) dbObject));
+        }
+        return holdings;
+    }
+
+    public void createNewHoldings(String body) {
+        Holding holding = new Gson().fromJson(body, Holding.class);
+        collection.insert(new BasicDBObject("title", holding.getTitle()).append("done", holding.isDone()).append("createdOn", new Date()));
+    }
+
+    public Holding find(String id) {
+        return new Holding((BasicDBObject) collection.findOne(new BasicDBObject("_id", new ObjectId(id))));
+    }
+
+    public Holding update(String todoId, String body) {
+        Holding holding = new Gson().fromJson(body, Holding.class);
+        collection.update(new BasicDBObject("_id", new ObjectId(todoId)), new BasicDBObject("$set", new BasicDBObject("done", holding.isDone())));
+        return this.find(todoId);
+    }
+}
