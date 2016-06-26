a8bc28e494d125f08da62fe44130dc9f1ba6abad
Giancarlo Angulo <igan.long@gmail.com>
2016-06-26 22:39:31 +0800

    Fix for headers in most pages.
    Fix for headers in most pages.

--- src/main/resources/public/scripts/app.js
+++ src/main/resources/public/scripts/app.js
@@ -33,6 +33,11 @@
                 url: '/orderz/create',
                 templateUrl:  'views/order/create.html',
                 controller: 'OrderCreateCtrl'
+            })
+            .state('traderconsole', {
+                url: '/trader/view',
+                templateUrl:  'views/trader/view.html',
+                controller: 'TraderViewCtrl'
             });
     }
 
@@ -111,6 +116,47 @@
                 console.log('status changed');
             }).error(function (data, status) {
                 console.log('Error ' + data)
+            })
+        }
+    });
+
+    app.controller('TraderViewCtrl', function ($scope, $http, $location) {
+        $scope.order = {
+            done: false,
+            type: 'buy',
+            stock: 'MEG',
+            price: '4.53',
+            cash: '77.00',
+            quantity: '',
+            total: '',
+            buyingPower: '77.00'
+        };
+
+        $scope.$watch('order.total', function(newVal, oldVal) {
+            if($scope.order.type == 'buy') {
+                $scope.order.quantity = newVal/$scope.order.price/1.005;
+            }
+        });
+
+        $scope.$watch('order.quantity', function(newVal, oldVal) {
+            if($scope.order.type == 'sell') {
+                $scope.order.total = newVal*$scope.order.price*0.99;
+            }
+        });
+
+        $scope.changeTransType = function (type) {
+            $scope.order.quantity = '';
+            $scope.order.total = '';
+        };
+
+        $scope.createOrder = function () {
+            alert($scope.order);
+            console.log($scope.order);
+            $http.post('/api/v1/orderz', $scope.order).success(function (data) {
+                $location.path('/orderz/list');
+            }).error(function (data, status) {
+                console.log('Error ' + data)
+                alert('Error ' + data);
             })
         }
     });
